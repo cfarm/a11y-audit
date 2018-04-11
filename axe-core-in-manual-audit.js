77b6560f480954730090b6366efd038e419e1f40
Catherine Farman <Catherine.Farman@cfpb.gov>
2018-04-11 15:39:10 -0400

    use rules object to track rules in audit, links, and audit item number

--- axe-core-in-manual-audit.js
+++ axe-core-in-manual-audit.js
@@ -1,176 +1,107 @@
-var listRulesNOTinAudit = [];
-
-var axeRules = [
-    {
-        axeName: 'aria-roles',
-        auditChecklistItems: ['A1']
-    },
-    {
-        axeName: 'aria-valid-attr',
-        auditChecklistItems: ['A1']
-    },
-    {
-        axeName: 'aria-valid-attr-value',
-        auditChecklistItems: ['A1']
-    },
-    {
-        axeName: 'audio-caption',
-        auditChecklistItems: ['B2', 'B6']
-    },
-    {
-        axeName: 'button-name',
-        auditChecklistItems: ['A6']
-    },
-    {
-        axeName: 'bypass',
-        auditChecklistItems: ['A12', 'B5', 'C4']
-    },
-    {
-        axeName: 'color-contrast',
-        auditChecklistItems: ['A15', 'E1']
-    },
-    {
-        axeName: 'document-title',
-        auaxeNameditChecklistItems: ['A4']
-    },
-    {
-        axeName: 'frame-title',
-        auditChecklistItems: ['E9']
-    },
-    // {
-    //     axeName: 'heading-order',
-    //     auditChecklistItems: ['A14'] // doesn't report accurately
-    // },
-    {
-        axeName: 'html-has-lang',
-        auditChecklistItems: ['A5']
-    },
-    {
-        axeName: 'html-lang-valid',
-        auditChecklistItems: ['A1', 'A5']
-    },
-    {
-        axeName: 'image-alt',
-        auditChecklistItems: ['A10','B2','B6']
-    },
-    {
-        axeName: 'label',
-        auditChecklistItems: ['A2']
-    },
-    {
-        axeName: 'link-in-text-block',
-        auditChecklistItems: ['E2']
-    },
-    {
-        axeName: 'meta-viewport',
-        auditChecklistItems: ['D3']
-    },
-    {
-        axeName: 'meta-viewport-large',
-        auditChecklistItems: ['D3']
-    },
-    {
-        axeName: 'object-alt',
-        auditChecklistItems: ['B2','B6']
-    },
-    // {
-    //     axeName: 'page-has-heading-one',
-    //     auditChecklistItems: ['A7'] // doesn't report accurately
-    // },
-    // {
-    //     axeName: 'skip-link',
-    //     auditChecklistItems: ['A12', 'B5'] // doesn't report accurately
-    // },
-    {
-        axeName: 'valid-lang',
-        auditChecklistItems: ['A1']
-    },
-    {
-        axeName: 'video-caption',
-        auditChecklistItems: ['B2', 'B6']
-    },
-    {
-        axeName: 'video-description',
-        auditChecklistItems: ['B2', 'B6']
-    },
-]
-
-var listRulesInAudit = ['aria-roles',
-'aria-valid-attr',
-'aria-valid-attr-value',
-'audio-caption',
-'button-name',
-'bypass',
-'color-contrast',
-'document-title',
-'frame-title',
-'heading-order',   
-'html-has-lang',
-'html-lang-valid',
-'image-alt',
-'label',
-'link-in-text-block',
-'meta-viewport',
-'meta-viewport-large',
-'object-alt',
-'page-has-heading-one',
-'skip-link',
-'valid-lang',
-'video-caption',
-'video-description'];
+var rulesInAudit = {
+    'aria-roles': { auditItemNumber: ['A1'] },
+    'aria-valid-attr': { auditItemNumber: ['A1'] },
+    'aria-valid-attr-value': { auditItemNumber: ['A1'] },
+    'audio-caption': { auditItemNumber: ['B2', 'B6'] },
+    'button-name': { auditItemNumber: ['A6'] },
+    'bypass': { auditItemNumber: ['A12', 'B5', 'C4'] },
+    'color-contrast': { auditItemNumber: ['A15', 'E1'] },
+    'document-title': { auditItemNumber: ['A4'] },
+    'frame-title': { auditItemNumber: ['E9'] },
+    // 'heading-order': { auditItemNumber: ['A14'] }, // doesn't report accurately
+    'html-has-lang': { auditItemNumber: ['A5'] },
+    'html-lang-valid': { auditItemNumber: ['A1', 'A5'] },
+    'image-alt': { auditItemNumber: ['A10','B2','B6'] },
+    'label': { auditItemNumber: ['A2'] },
+    'link-in-text-block': { auditItemNumber: ['E2'] },
+    'meta-viewport': { auditItemNumber: ['D3'] },
+    'meta-viewport-large': { auditItemNumber: ['D3'] },
+    'object-alt': { auditItemNumber: ['B2','B6'] },
+    // 'page-has-heading-one': { auditItemNumber: ['A7'] }, // doesn't report accurately
+    // 'skip-link': { auditItemNumber: ['A12', 'B5'] }, // doesn't report accurately
+    'valid-lang': { auditItemNumber: ['A1'] },
+    'video-caption': { auditItemNumber: ['B2', 'B6'] },
+    'video-description': { auditItemNumber: ['B2', 'B6'] }
+};
 
 var elRulesInAudit = document.getElementById('rules-in-audit');
 var elRulesNOTinAudit = document.getElementById('rules-NOT-in-audit');
 var elAllRules = document.querySelectorAll('#all-rules li');
 
-function addToPage(rulesList, el) {
-    for (var i = 0; i < rulesList.length; i++) {
-        el.innerHTML += '<li>' + rulesList[i] + '</li>';
-    }
-}
-
-// addToPage(listRulesInAudit, elRulesInAudit);
-
 function getAllRules(els) {
     var rulesList = [];
     for (var i = 0; i < els.length; i++) {
         var ruleLink = els[i].innerHTML;
-        // var slug = ruleLink.replace(/https:\/\/dequeuniversity.com\/rules\/axe\/2.6\//, '');
         rulesList.push(ruleLink);
     }
-    console.log(rulesList);
     return rulesList;
 }
 
-function filterList(originalList, filterOutList) {
+function filterList(originalList, filterOutObj) {
     var result = {};
 
-    const notInList = value =>
-      !filterOutList.some(element => value.includes(element));
+    var filterOutList = Object.keys(filterOutObj);
 
-    const inList = value =>
-      filterOutList.some(element => value.includes(element));
+    result.inAudit = originalList.filter( function( link ) {
+        var rulesInList = null;
+        
+        rulesInList = filterOutList.some( function (element, index, array) {
+            
+            // match element preceded by 2.6 in url with a quote at the end
+            if ( link.includes( '2.6/' + element + '"', index ) ) {
+                var rule = filterOutObj[array[index]];
+                rule.href = link;
+            }
 
-    result.notInList = originalList.filter(notInList);
-    result.inList = originalList.filter(inList);
+            return link.includes( '2.6/' + element + '"' );
+        } );
 
-    console.log(result);
+        return rulesInList; 
+    } );
+
+    const notInAudit = value =>
+        !result.inAudit.some(element => value.includes(element));
+
+    result.notInAudit = originalList.filter(notInAudit);
+
+    console.log( result );
     return result;
     
 }
 
-function filterRules() {
+function formatAuditItemNumber(itemList) {
+    var auditText = '';
+    itemList.map( function( value, index ) {
+        if ( itemList.length > 2 && index === itemList.length - 1 || itemList.length === 2 && index === 1 ) {
+            auditText += 'and  ' + value + '.';            
+        } else if ( itemList.length === 2 && index === 0) {
+            auditText += value + ' ';            
+        } else if (itemList.length === 1) {
+            auditText += value + '.';            
+        }
+        else {
+            auditText += value + ', ';            
+        }
+    } );
+    return auditText;
+}
 
-    var listAllRules = getAllRules(elAllRules);
-    var listRulesNOTinAudit = filterList(listAllRules, listRulesInAudit).notInList;
-    var linksInAudit = filterList(listAllRules, listRulesInAudit).inList;
+function filterRules( ) {
+    var listAllRules = getAllRules( elAllRules );
+    var filteredRulesList = filterList( listAllRules, rulesInAudit );
+    var listRulesNOTinAudit = filteredRulesList.notInAudit;
+    var linksInAudit = filteredRulesList.inAudit;
 
     for (var i = 0; i < listRulesNOTinAudit.length; i++) {
         elRulesNOTinAudit.innerHTML += '<li>' + listRulesNOTinAudit[i] + '</li>';
     }
 
-    for (var i = 0; i < linksInAudit.length; i++) {
-        elRulesInAudit.innerHTML += '<li>' + linksInAudit[i] + '</li>';
+    var rulesInAuditList = Object.keys( rulesInAudit );
+
+    for (var i = 0; i < rulesInAuditList.length; i++) {
+        var rule = rulesInAudit[rulesInAuditList[i]];
+
+        elRulesInAudit.innerHTML += '<li>Violation of ' + rule.href + ' is a <strong>failure for ' + formatAuditItemNumber(rule.auditItemNumber) + ' </strong></li>';
     }
 
 }
